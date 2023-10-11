0b9b2729f3703538188ef57b6749a5c0fd2971af
Wadad Parker <wadadparker5@gmail.com>
2023-10-11 15:39:19 +0530

    feat: savings tab

--- src/pages/savings/Savings.jsx
+++ src/pages/savings/Savings.jsx
@@ -1,9 +1,55 @@
 import styles from "./savings.module.css";
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { fetchSavings,addNewSavings } from "../../actions";
+
+import { FilterBar } from "../../components/filterBar/FilterBar";
+import { IncomeExpenseForm } from "../../components/form/IncomeExpenseForm";
+import { DataTable } from "../../components/table/DataTable";
+import { Spinner } from "../../components/spinner/Spinner";
 
 export const Savings = () => {
+
+  const [filter,setFilter]=useState({sort:false,category:""});
+
+  const savings = useSelector((state)=>state.savings)
+  const loading = useSelector((state)=>state.loading);
+  const dispatch = useDispatch();
+
+  const applyFilters=()=>
+  {
+    if(filter.sort)
+    {
+      const sortedSavings =  [...savings]?.sort((a,b)=>a.amount-b.amount)
+      return sortedSavings?.filter(({category})=>category.includes(filter.category));
+    }
+    return [...savings]?.filter(({category})=>category.includes(filter.category));
+  }
+
+  const filteredSavings = applyFilters();
+
+  const formHandler=(newSavings)=>
+  {
+    dispatch(addNewSavings(newSavings));
+  }
+
+  useEffect(()=>
+  {
+    dispatch(fetchSavings());
+  },[dispatch])
+
+
   return (
-    <div>Savings</div>
+    <div className={styles[`main-container`]}>
+      <h1>Savings</h1>
+      <FilterBar categories={savings} filter={filter} setFilter={setFilter}/>
+      <section className={styles[`table-and-form`]}>
+        {loading?<Spinner />
+        :<DataTable list={filteredSavings}/>}
+        <IncomeExpenseForm type="Savings" formHandler={formHandler}/>
+      </section>
+    </div>
   )
 }
