8e44db20b05d3448f1d10fdec48f9e412584bfed
Wadad Parker <wadadparker5@gmail.com>
2023-10-11 21:50:21 +0530

    feat: Home and Reports

--- src/components/table/DataTable.jsx
+++ src/components/table/DataTable.jsx
@@ -3,10 +3,27 @@ import styles from "./table.module.css";
 import React from 'react'
 
 export const DataTable = ({list}) => {
+
+
+    const convertToDate=(dateString)=>
+    {
+    const dateObject = new Date(dateString);
+    
+    const day = dateObject.getDate();
+    const month = dateObject.toLocaleString('default', { month: 'short' });
+    const year = dateObject.getFullYear();
+    
+    const formattedDate = `${day} ${month} ${year}`;
+    
+    return formattedDate;
+    }
+    
+
   return (
 <table className={styles.table}>
     <thead className={styles['heading-row']} key={"table"}>
         <tr>
+            <th>Date</th>
             <th>Category</th>
             <th>Description</th>
             <th>Amount</th>
@@ -16,12 +33,21 @@ export const DataTable = ({list}) => {
         {list.map(item =>
             (
                 <tr key={item?._id}>
+                    <td>{convertToDate(item?.createdAt)}</td>
                     <td>{item?.category}</td>
                     <td>{item?.description}</td>
                     <td>{item?.amount}</td>
                 </tr>
             ))}
     </tbody>
+    <tfoot>
+        <tr>
+            <td></td>
+            <td></td>
+            <td className={styles.total}>Total</td>
+            <td> <b>{list?.reduce((acc,{amount})=>acc + amount,0)}</b></td>
+        </tr>
+    </tfoot>
     </table>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,53 @@
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./home.module.css";
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { fetchIncome,fetchExpenses,fetchSavings } from "../../actions";
+import { IncomeExpenseReport } from "../../components/incomeExpenseReport/IncomeExpenseReport";
+import { ExpenseReport } from "../../components/expenseReport/ExpenseReport";
 
 export const Home = () => {
+
+  const [reportType,setReportType]= useState("income");
+  const [showReport,setShowReport] = useState(false);
+
+  const income = useSelector((state)=>state.income);
+  const expenses = useSelector((state)=>state.expenses);
+  const savings = useSelector((state)=>state.savings);
+  const dispatch = useDispatch();
+
+  const totalIncome = income?.reduce((acc,{amount})=>acc + amount,0);
+  const totalExpense = expenses?.reduce((acc,{amount})=>acc + amount,0);
+  const totalSavings = savings?.reduce((acc,{amount})=>acc + amount,0);
+
+  const changeHandler=(option)=>
+  {
+    setReportType(option)
+    setShowReport(false);
+  }
+
+  useEffect(()=>
+  {
+    dispatch(fetchIncome());
+    dispatch(fetchExpenses());
+    dispatch(fetchSavings());
+  },[dispatch])
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Finance Manager</h1>
+      <section className={styles.container}>
+        <label htmlFor="dropdown">Select Report Type:</label>
+        <select value={reportType} id="dropdown" onChange={(e)=>changeHandler(e.target.value)}>
+          <option value={"income"}>Income vs Expense</option>
+          <option value={"expense"}>Expense Breakdown</option>
+        </select>
+        <button className={styles[`generate-report-button`]} onClick={()=>setShowReport(true)}>Generate Report</button>
+      </section>
+
+      {showReport && reportType==="income" && <IncomeExpenseReport totalIncome={totalIncome} totalExpense={totalExpense} totalSavings={totalSavings}/>}
+      {showReport && reportType==="expense" &&<ExpenseReport expenses={expenses} totalExpense={totalExpense}/>}
+    </div>
   )
 }
